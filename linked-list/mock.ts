1d7c35e2985785bb86285a618fcb1fd6351619b0
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2022-10-23 09:58:27 +0500

    added mock data

---
+++ linked-list/mock.ts
@@ -0,0 +1,10 @@
+import { LinkedList } from "./types";
+
+// dummy llist
+export const dummy_llist = new LinkedList();
+// insert elements inside dummy llist
+dummy_llist.insert_node(1);
+dummy_llist.insert_node(2);
+dummy_llist.insert_node(3);
+dummy_llist.insert_node(4);
+dummy_llist.insert_node(5);
